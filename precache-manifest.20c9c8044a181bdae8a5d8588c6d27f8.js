4b6da96971904ad54163642fc7f9f9fe160e1285
fsz0826 <7708764+fsz0826@user.noreply.gitee.com>
2020-07-20 10:16:11 +0800

    deploy

---
+++ precache-manifest.20c9c8044a181bdae8a5d8588c6d27f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b980a096b72b41234ef6",
+    "url": "/Bookkeeping/css/app.07bf7d4f.css"
+  },
+  {
+    "revision": "2bdb40ea9b71cd7e60eae94c9f316180",
+    "url": "/Bookkeeping/index.html"
+  },
+  {
+    "revision": "b980a096b72b41234ef6",
+    "url": "/Bookkeeping/js/app.ff67c286.js"
+  },
+  {
+    "revision": "c2caad7614f0f399916f",
+    "url": "/Bookkeeping/js/chunk-vendors.f383f3d2.js"
+  },
+  {
+    "revision": "6f5da2ed20f30fd514f3b10afa2a3add",
+    "url": "/Bookkeeping/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Bookkeeping/robots.txt"
+  }
+]);
